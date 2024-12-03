2c7b76e71a4d76cb52d9ad200b89d0aaa2890960
Marcee09 <marcela.ah96@gmail.com>
2024-12-03 18:46:56 -0300

    eliminar el contador

--- src/routes/web.php
+++ src/routes/web.php
@@ -47,55 +47,8 @@ Route::middleware('permission:see-panel')->group(function () {
 
 });
 
-// Rutas de la aplicación
 
-// // // Ruta para el contador inicial
-// Route::get('/contador', function () {
-//     return view('contador', ['número' => 0]);
-// })->name('contador');
 
-// // Ruta para incrementar el contador con validación para no superar 10
-// Route::get('/contador/{número}/inc', function ($número) {
-//     if ($número < 10) {
-//         $número++;
-        
-//     }
-//     return view('contador', ['número' => $número]);
-// })->name('contador.inc');
-
-// // Ruta para decrementar el contador con validación para no bajar de 0
-// Route::get('/contador/{número}/dec', function ($número) {
-//     if ($número > 0) {
-//         $número--;
-//     }
-//     return view('contador', ['número' => $número]);
-// })->name('contador.dec');
-
-// // Ruta para reiniciar el contador a cero
-// Route::get('/contador/reset', function () {
-//     return view('contador', ['número' => 0]);
-// })->name('contador.reset');
-
-// // Ruta para duplicar el valor del contador, con límite de 10
-// Route::get('/contador/{número}/duplicate', function ($número) {
-//     $número = min($número * 2, 10); // Duplicar, pero máximo 10
-//     return view('contador', ['número' => $número]);
-// })->name('contador.duplicate');
-
-// Ruta para restablecer el contador a un valor específico
-Route::get('/contador/set/{número}', function ($número) {
-    return view('contador', ['número' => $número]);
-})->name('contador.set');
-
-Route::get('/contador', function () {
-    return view('contador', ['número' => 0]);
-})->name('contador');
-
-// Cambiamos las rutas para que llamen al controlador
-Route::get('/contador/{número}/inc', [ContadorController::class, 'incrementar'])->name('contador.inc');
-Route::get('/contador/{número}/dec', [ContadorController::class, 'decrementar'])->name('contador.dec');
-Route::get('/contador/reset', [ContadorController::class, 'reset'])->name('contador.reset');
-Route::get('/contador/{número}/duplicate', [ContadorController::class, 'duplicar'])->name('contador.duplicate');
 Route::resource('assets', AssetController::class)->middleware('auth');
 Route::post('/tickets/{ticket}/assign-area', [TicketController::class, 'assignToArea'])->name('tickets.assignArea');
 
