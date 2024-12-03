c54e12663e0e3fee4ae9096b758aafde2af8e721
Marcee09 <marcela.ah96@gmail.com>
2024-12-03 18:43:20 -0300

    componentes

--- src/resources/views/components/welcome.blade.php
+++ src/resources/views/components/welcome.blade.php
@@ -2,11 +2,11 @@
     class="p-6 lg:p-8 bg-white dark:bg-gray-800 dark:bg-gradient-to-bl dark:from-gray-700/50 dark:via-transparent border-b border-gray-200 dark:border-gray-700">
     <x-application-logo class="block h-12 w-auto" />
 
-    <h1 class="mt-8 text-2xl font-medium text-gray-900 dark:text-white">
+    <h1 class="mt-8 text-2xl font-medium text-gray-900 dark:text-black">
         {{ __('welcome_session.title') }}
     </h1>
 
-    <p class="mt-6 text-gray-500 dark:text-gray-400 leading-relaxed">
+    <p class="mt-6 text-gray-800 dark:text-gray-800 leading-relaxed">
         {{ __('welcome_session.subtitle') }}
     </p>
 </div>
@@ -19,19 +19,19 @@
                 <path stroke-linecap="round" stroke-linejoin="round"
                     d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25" />
             </svg>
-            <h2 class="ms-3 text-xl font-semibold text-gray-900 dark:text-white">
+            <h2 class="ms-3 text-xl font-semibold text-gray-900 dark:text-black">
                 <a href="https://laravel.com/docs">Documentation</a>
             </h2>
         </div>
 
-        <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
+        <p class="mt-4 text-gray-800 dark:text-gray-900 text-sm leading-relaxed">
             Laravel has wonderful documentation covering every aspect of the framework. Whether you're new to the
             framework or have previous experience, we recommend reading all of the documentation from beginning to end.
         </p>
 
         <p class="mt-4 text-sm">
             <a href="https://laravel.com/docs"
-                class="inline-flex items-center font-semibold text-indigo-700 dark:text-indigo-300">
+                class="inline-flex items-center font-semibold text-indigo-700 dark:text-indigo-900">
                 Explore the documentation
 
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
@@ -51,12 +51,12 @@
                 <path stroke-linecap="round"
                     d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z" />
             </svg>
-            <h2 class="ms-3 text-xl font-semibold text-gray-900 dark:text-white">
+            <h2 class="ms-3 text-xl font-semibold text-gray-900 dark:text-black">
                 <a href="https://laracasts.com">Laracasts</a>
             </h2>
         </div>
 
-        <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
+        <p class="mt-4 text-gray-800 dark:text-gray-900 text-sm leading-relaxed">
             Laracasts offers thousands of video tutorials on Laravel, PHP, and JavaScript development. Check them out,
             see for yourself, and massively level up your development skills in the process.
         </p>
@@ -88,7 +88,7 @@
             </h2>
         </div>
 
-        <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
+        <p class="mt-4 text-gray-800 dark:text-gray-900 text-sm leading-relaxed">
             Laravel Jetstream is built with Tailwind, an amazing utility first CSS framework that doesn't get in your
             way. You'll be amazed how easily you can build and maintain fresh, modern designs with this wonderful
             framework at your fingertips.
